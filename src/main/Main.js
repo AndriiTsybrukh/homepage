30e645b6cddc7eda5c08e74ea514c8220931fc9a
Andrii <newsensys@gmail.com>
2019-05-13 22:37:59 +0300

    Add base files

---
+++ src/main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.css';
+import PhotoBlock from './photo_block/PhotoBlock';
+
+const Main = () => (
+    <div className="main-body">
+      <PhotoBlock />
+    </div>
+);
+
+export default Main;
