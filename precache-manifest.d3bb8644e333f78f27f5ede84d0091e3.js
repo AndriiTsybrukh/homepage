3c4241bedeab909c25df309c767a3b11caa8f38a
Andrii <newsensys@gmail.com>
2019-05-13 23:36:38 +0300

    Updates

---
+++ precache-manifest.d3bb8644e333f78f27f5ede84d0091e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d16fa16046c9997ab474107c6dc386",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "277cd58debd5f1c09c7d",
+    "url": "/homepage/static/css/main.a11ae455.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/homepage/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "277cd58debd5f1c09c7d",
+    "url": "/homepage/static/js/main.5d3aeac9.chunk.js"
+  },
+  {
+    "revision": "1d55219e7988c0454e32",
+    "url": "/homepage/static/js/runtime~main.336ecad5.js"
+  }
+]);
