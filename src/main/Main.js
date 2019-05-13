e0b22ebf20d714555674e96bd21b319499bb8634
Andrii <newsensys@gmail.com>
2019-05-14 00:49:23 +0300

    Add advanced info with description and languages blocks

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Main.css';
 import PhotoBlock from './photo_block/PhotoBlock';
 import ShortInfo from '../short_info/ShortInfo';
+import AdvancedInfo from '../advanced_info/AdvancedInfo';
 
 const Main = () => (
     <div className="main-body">
       <PhotoBlock />
       <ShortInfo />
+      <AdvancedInfo />
     </div>
 );
 
