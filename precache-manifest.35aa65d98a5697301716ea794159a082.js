3b0e4c089e7887d5a8e121d1a69763d7d13b8bfb
Andrii <newsensys@gmail.com>
2019-06-11 15:49:52 +0300

    Updates

--- precache-manifest.35aa65d98a5697301716ea794159a082.js
+++ precache-manifest.35aa65d98a5697301716ea794159a082.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fe135b8d4321539833071f63fa1cec6a",
+    "revision": "c15b4b7062e06124a336f15d6a521814",
     "url": "/homepage/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/homepage/static/css/2.400ebc83.chunk.css"
   },
   {
-    "revision": "13774165c674058d8ddb",
-    "url": "/homepage/static/css/main.42a5c547.chunk.css"
+    "revision": "5556e73805e694047081",
+    "url": "/homepage/static/css/main.f821a206.chunk.css"
   },
   {
     "revision": "6e0afd647140a01ccaf7",
     "url": "/homepage/static/js/2.d394458f.chunk.js"
   },
   {
-    "revision": "13774165c674058d8ddb",
-    "url": "/homepage/static/js/main.8e0fdb39.chunk.js"
+    "revision": "5556e73805e694047081",
+    "url": "/homepage/static/js/main.8788a28d.chunk.js"
   },
   {
     "revision": "1d55219e7988c0454e32",
